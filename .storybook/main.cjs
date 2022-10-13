bfa99c9e9ea5cd8e0e8f48ddee1ff92db6f1d49b
Matheus Pereira <mat_pereiraa@hotmail.com>
2022-10-13 12:29:36 -0300

    Alter ViteFinal in main.cjs Storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, {configType}) => {
     if(configType == "PRODUCTION"){
-      config.base = '/ignite-lab-design-system' // Essa configuração só será utilizada em caso de o storybook for acessado por subdominio exemplo.com/ignite-lab-design-system para o storybook não se perder.
+      config.base = '/DesignSystem-IgniteLab' // Essa configuração só será utilizada em caso de o storybook for acessado por subdominio exemplo.com/ignite-lab-design-system para o storybook não se perder.
     }
 
     return config
